16fbbc635e069c2663863bec1422712112c4ecbe
nathanielkess <nathaniel@nathanielkessler.com>
2017-03-26 12:40:25 -0400

    auth listener style v5 35:28

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,23 +2,16 @@ import { auth, googleAuthProvider } from '../firebase';
 import { addUser } from './users';
 
 export const signIn = () => {
-
   return (dispatch) => {
     dispatch({type: 'ATTEMPTING_LOGIN'});
-    auth.signInWithPopup(googleAuthProvider).then(({ user }) => {
-      dispatch(signedIn(user));
-      dispatch(addUser(user));
-    });
+    auth.signInWithPopup(googleAuthProvider);
   }
-
 };
 
 export const signOut = () => {
   return (dispatch) => {
     dispatch({type: 'ATTEMPTING_LOGIN'});
-    auth.signOut().then(() => {
-      dispatch(signedOut());
-    })
+    auth.signOut()
   }
 };
 
@@ -35,5 +28,18 @@ const signedIn = (user) => {
 const signedOut = () =>{
   return {
     type: 'SIGN_OUT',
+  }
+}
+
+export const startListeningToAuthChanges = () => {
+  return (dispatch) => {
+    auth.onAuthStateChanged((user) => {
+      if(user){
+        dispatch(signedIn(user));
+        dispatch(addUser(user));
+      }else{
+        dispatch(signedOut());
+      }
+    });
   }
 }
